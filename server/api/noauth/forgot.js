52de732fdfb279e490232a26fe8be7ba963a61a2
Ali Iftikhar <ali.iftikhar@incedo.de>
2016-06-30 16:19:51 +0200

    have to complete Nodemailer

--- server/api/noauth/forgot.js
+++ server/api/noauth/forgot.js
@@ -1,3 +1,19 @@
+import User from '../../models/user'
+import nodemailer from 'nodemailer'
+
 export default ({ body }, res) => {
-    
+  let user = body.user
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (!user) {
+      req.flash('error', 'No account with that email address exists.')
+      return res.json({success: false, message: 'no such email'})
+    }
+    var smtpTransport = nodemailer.createTransport('SMTP', {
+      service: 'Gmail',
+      auth: {
+        user: '!!! YOUR SENDGRID USERNAME !!!',
+        pass: '!!! YOUR SENDGRID PASSWORD !!!'
+      }
+    })
+  })
 }
